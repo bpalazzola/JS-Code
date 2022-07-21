30c590a0677cb84082ef483e218f36b0e6f49a87
Brian Palazzola <bpalazzola@ymail.com>
2022-07-21 17:44:23 -0500

    First practice commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+let name = 'Brian Palazzola is here';
+console.log(name);
+
+let firstName = 'George';
+console.log(firstName);
+
+let interestRate = .3;
+interestRate = 1;
+console.log(interestRate);
+
+
+
